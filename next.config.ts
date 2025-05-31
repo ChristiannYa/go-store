97f64ea920fa6a543f4ae44dbc81766f4e2e42f8
ChristiannYa <christian.ylopez02@gmail.com>
2025-05-30 22:37:06 -0500

    improved development and UI

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["10.0.0.4:3000", "localhost:3000"],
 };
 
 export default nextConfig;
--- src/components/ServerCheckingPage.tsx
+++ src/components/ServerCheckingPage.tsx
@@ -1,7 +1,11 @@
 export function ServerCheckingPage() {
   return (
     <div className="page">
-      <div className="w-8 h-8 border-[3px] border-blue-400 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
+      <h1 className="text-3xl">
+        <span className="text-sky-400">Golang</span>{" "}
+        <span className="underline">Authentication!</span>
+      </h1>
+      <div className="w-8 h-8 border-[3px] border-blue-400 border-t-transparent rounded-full animate-spin mx-auto mt-6"></div>
     </div>
   );
 }
